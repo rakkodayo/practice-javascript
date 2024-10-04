c9193519e8028cf2335a37072816e55123eb999a
rakkodayo <seaotter7293@gmail.com>
2024-10-04 04:58:15 +0000

    10_3

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-document.querySelector("h1").textContent = "自分の名前"
-document.querySelector("h2").textContent = "adachi"
+function errorInput(){
+console.log(document.getElementById("month").value)
+console.log(document.getElementById("day").value)
 
-//documentとは！！
-/* htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+const month = document.getElementById("month").value;
+const day = document.getElementById("day").value;
 
-//DOM(ドム)
-/*ドキュメントオブジェクトモデル （Document Object Model）の略で、
-Webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
-
-//上記コードの意味
-//document.querySelector("h1").textContent = "自分の名前"
-
-//ドキュメントの中の("h1")タグを探してね
-//document.querySelector("h1")
-
-// h1タグの中身のtextを変えてね
-//.textContent = "自分の名前"
+console.log("month:",month,"day:",day)
+}
