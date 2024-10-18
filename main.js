3076f00b6887d3c131f78f37c662e678c0cd3a4a
rakkodayo <seaotter7293@gmail.com>
2024-10-18 06:18:21 +0000

    10月18日ゲームを作った

--- main.js
+++ main.js
@@ -1,24 +1,86 @@
-function errorInput(){
-const month = document.getElementById("month").value.trim();
-const day = document.getElementById("day").value.trim();
-if(month<1 || month>12){
-    alert("適切な値じゃねぇよ")
-    return
-}
-if(day<1 || month>31){
-    alert("適切な値じゃねぇよ")
-    return
-}
+const gameSelector = document.getElementById("game-selector");
+const gameContainer = document.getElementById("game-container");
 
-console.log("month:",month,"day:",day)
-}
-const monthSelect = document.getElementById("month2")
-
-monthSelectList();{
-    for (let i=1;i<=12;i++){
-        let option = document.createElement("option");
-        option.value = i;
-        option.textContent = i + "月"; 
-        monthSelect.appendChild("option");
+gameSelector.addEventListener("change",function(){
+    gameContainer.innerHTML = "";
+    switch (gameSelector.value) {
+        case "none":
+            gameContainer.textContent = "ゲームを選ぶとここに表示されます"
+            break;
+        case "click-counter":
+            Clickcountergame();
+            break;
+        case "number-guess":
+            Numberguessgame();
+            break;
     }
+})
+
+
+
+function Numberguessgame(){
+    const randomNumber = Math.floor(Math.random()*100)+1;
+    
+    let message = document.createElement("p");
+    let input = document.createElement("input");
+    input.type = "number"
+    input.max = 100
+    input.min = 1
+    input.placeholder = "好きな数字を入力してください (1-100)"
+    let button = document.createElement("button");
+    button.textContent = "予想";
+    
+    
+    button.addEventListener("click",function(){
+        const i = parseInt(input.value);
+        if (randomNumber < i) {
+            message.textContent = "もっと小さいよ"
+        }else if (randomNumber > i) {
+            message.textContent = "もっと大きいよ"
+        }else if (randomNumber === i) {
+            message.textContent = "正解！"
+        }
+        
+    })
+    gameContainer.appendChild(input);
+    gameContainer.appendChild(button);
+    gameContainer.appendChild(message);
 }
+
+
+
+
+function Clickcountergame(){
+    let count = 0;
+    
+    let button = document.createElement("button");
+    button.textContent = "+1ボタン";
+    
+    let button1 = document.createElement("button");
+    button1.textContent = "+10ボタン";
+    
+    let button2 = document.createElement("button");
+    button2.textContent = "+100ボタン";
+    let counter = document.createElement("p");
+    counter.textContent = count;
+    button.addEventListener("click",function(){
+        count++;
+        counter.textContent = count;
+    })
+    button1.addEventListener("click",function(){
+        count = count + 10;
+        counter.textContent = count;
+    })
+    button2.addEventListener("click",function(){
+        count = count + 100;
+        counter.textContent = count;
+    })
+    gameContainer.appendChild(button);
+    gameContainer.appendChild(button1);
+    gameContainer.appendChild(button2);
+    gameContainer.appendChild(counter);
+
+}
+
+
+
