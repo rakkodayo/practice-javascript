5d426590e077696fbc16bab43d9dd10c94f2cb10
rakkodayo <seaotter7293@gmail.com>
2025-01-16 05:21:12 +0000

    1_16ボタンの追加

--- main.js
+++ main.js
@@ -4,7 +4,13 @@
     
     let button1 = document.createElement("button");
     button1.textContent = "+1ボタン";
+
+    let button2 = document.createElement("button");
+    button2.textContent = "+10ボタン";
     
+    let button3 = document.createElement("button");
+    button3.textContent = "RESET";
+
     let counter = document.createElement("p");
     counter.textContent = count;
     
@@ -12,8 +18,20 @@
         count++;
         counter.textContent = count;
     })
+
+    button2.addEventListener("click",function(){
+        count = count + 10;
+        counter.textContent = count;
+    })
+
+    counter.addEventListener("click",function(){
+        count = 0;
+        counter.textContent = count;
+    })
     
     gameContainer.appendChild(button1);
+    gameContainer.appendChild(button2);
+    //gameContainer.appendChild(button3);
     gameContainer.appendChild(counter);
 
 //}
